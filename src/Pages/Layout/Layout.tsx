7c2c69edc43dfb4784212391359d5b97b34ff9ae
nepolionchakma <nepolionchakma.nc@gmail.com>
2025-03-20 10:19:02 +0600

    change name

--- src/Pages/Layout/Layout.tsx
+++ src/Pages/Layout/Layout.tsx
@@ -1,4 +1,4 @@
-import Navigation from "@/components/Navigation/Navigation";
+import Navigation from "@/components/Navigation/NavigationMenu";
 import { Outlet } from "react-router";
 
 const Layout = () => {
