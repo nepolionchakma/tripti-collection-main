68119425d4fb96b46a3b3851bc1f1fea3c2e3c4a
nepolionchakma <nepolionchakma.nc@gmail.com>
2025-05-09 10:31:42 +0600

    Admin menu added

--- src/Pages/Admin/Components/Sidebar/SidebarMenu.tsx
+++ src/Pages/Admin/Components/Sidebar/SidebarMenu.tsx
@@ -1,15 +1,17 @@
 import { useAdminContext } from "@/context/Admin/AdminContext";
 import { ChevronLeft, ChevronRight } from "lucide-react";
-import { Sidebar, Menu, MenuItem, SubMenu } from "react-pro-sidebar";
+import { Sidebar, Menu, MenuItem } from "react-pro-sidebar";
+import { Link } from "react-router";
+import { menuData } from "../../Menu/MenuData.tsx";
 
 const SidebarMenu = () => {
   const { isSidebarOpen, setIsSidebarOpen } = useAdminContext();
   return (
     <div className="relative">
-      <div className="absolute top-0 -right-2 z-50">
+      <div className="absolute top-0 -right-2 z-[60]">
         <button
           onClick={() => setIsSidebarOpen(!isSidebarOpen)}
-          className="cursor-pointer z-50 p-1 border rounded-full bg-white"
+          className="cursor-pointer p-1 border rounded-full bg-white"
         >
           {isSidebarOpen ? (
             <ChevronLeft size={13} />
@@ -23,23 +25,19 @@ const SidebarMenu = () => {
           <Sidebar
             collapsed={!isSidebarOpen}
             transitionDuration={1000}
-            className="h-[calc(100vh-3rem)] text-[14px] z-40"
+            className="h-[calc(100vh-3rem)] text-[14px]"
           >
             <Menu>
               <Menu>
-                <SubMenu label="Charts">
-                  <MenuItem> Pie charts</MenuItem>
-                  <MenuItem> Line charts</MenuItem>
-                  <MenuItem> Bar charts</MenuItem>
-                </SubMenu>
-                <SubMenu label="Maps">
-                  <MenuItem> Google maps</MenuItem>
-                  <MenuItem> Open street maps</MenuItem>
-                </SubMenu>
-                <SubMenu label="Theme">
-                  <MenuItem> Dark</MenuItem>
-                  <MenuItem> Light</MenuItem>
-                </SubMenu>
+                {menuData.map((item) => (
+                  <MenuItem
+                    key={item.id}
+                    component={<Link to={item.path}>{item.title}</Link>}
+                    icon={item.icon}
+                  >
+                    {item.title}
+                  </MenuItem>
+                ))}
               </Menu>
             </Menu>
           </Sidebar>
