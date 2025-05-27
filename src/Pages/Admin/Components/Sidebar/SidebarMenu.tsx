a232c6a18b2f0df4cf86aa502271ef20896746e2
nepolionchakma <nepolionchakma.nc@gmail.com>
2025-05-27 09:37:30 +0600

    table fixed

--- src/Pages/Admin/Components/Sidebar/SidebarMenu.tsx
+++ src/Pages/Admin/Components/Sidebar/SidebarMenu.tsx
@@ -3,13 +3,14 @@ import { ChevronLeft, ChevronRight } from "lucide-react";
 import { Sidebar, Menu, MenuItem } from "react-pro-sidebar";
 import { Link, useLocation } from "react-router";
 import { menuData } from "../../Menu/MenuData.tsx";
+import "../../sidebar-scrollbar.css";
 
 const SidebarMenu = () => {
   const { isSidebarOpen, setIsSidebarOpen } = useAdminContext();
   const searchParams = useLocation();
 
   return (
-    <div className="relative">
+    <div className="relative bg-amber-200">
       <div className="absolute top-0 -right-2 z-40">
         <button
           onClick={() => setIsSidebarOpen(!isSidebarOpen)}
@@ -27,7 +28,7 @@ const SidebarMenu = () => {
         transitionDuration={1000}
         className="
         h-[calc(100vh-3.1rem)]
-        text-[14px]"
+        text-[14px] "
       >
         <Menu>
           <Menu>
--- src/Pages/Admin/Page/ManageProducts/Table/ManageProductsTable.tsx
+++ src/Pages/Admin/Page/ManageProducts/Table/ManageProductsTable.tsx
@@ -98,7 +98,7 @@ export function ManageProductsTable() {
     "offer",
     "features",
     "description",
-    // "material",
+    "material",
   ];
 
   React.useEffect(() => {
